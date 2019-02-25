2d7b9031e67e6061a64d6f712f36258a183a2547
Shivam Singh <shivam.singh@joshtechnologygroup.com>
2019-02-25 13:48:22 +0000

    Fixed reviews

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var slider = tns({
+    container: '.my-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    mode: "gallery",
+    autoplayButtonOutput: false,
+    controls: false
+});
